8764b5bac35ba4aaa5e3e8bb163664ff61a37bba
sydykovtima <116348777+sydykovtima@users.noreply.github.com>
2022-12-26 13:43:33 +0600

    Update ViewController.swift

--- еуые/ViewController.swift
+++ еуые/ViewController.swift
@@ -1,42 +1,78 @@
 //
 //  ViewController.swift
-//  еуые
+//  homewrok2Month3
 //
-//  Created by Mac on 12/12/2022.
+//  Created by Mac on 25/12/2022.
 //
 
 import UIKit
 
-class ViewController: UIViewController  {
+class ViewController: UIViewController {
     
-    @IBOutlet weak var Image: UITableViewCell!
-    @IBOutlet weak var dfaadf: UITableViewCell!
-    @IBOutlet weak var ZametkaTableView: UITableView!
-    var zametki:  [String ] = []
+    @IBOutlet weak var view1: UIView!
+    
+    @IBOutlet weak var view2: UIView!
+    
+    @IBOutlet weak var view3: UIView!
+    
+    @IBOutlet weak var view4: UIView!
+    
+    @IBOutlet weak var item1: UIImageView!
+    
+    @IBOutlet weak var item2: UIImageView!
+    
+
+    @IBOutlet weak var item3: UIImageView!
+    
+    @IBOutlet weak var item4: UIImageView!
     
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        ZametkaTableView.register( UITableViewCell.self, forCellReuseIdentifier: "zametka_cell")
-        title = "Заметки"
-        navigationItem.rightBarButtonItem = UIBarButtonItem.init(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(rightBarButtonTap))
+        // Do any additional setup after loading the view.
+        view1.layer.cornerRadius = 20
+        item1.layer.cornerRadius = 30
+        item1.layer.borderWidth = 3
+        item1.layer.borderColor = UIColor.orange.cgColor
+        view2.layer.cornerRadius = 20
+        item2.layer.cornerRadius = 30
+        item2.layer.borderWidth = 3
+        item2.layer.borderColor = UIColor.blue.cgColor
+        view2.layer.borderWidth = 2
+        view2.layer.borderColor = UIColor.black.cgColor
+        view3.layer.cornerRadius = 20
+        view3.layer.borderWidth = 3
+        view3.layer.borderColor = UIColor.black.cgColor
+        item3.layer.cornerRadius = 30
+        item3.layer.borderWidth = 3
+        item3.layer.borderColor = UIColor.black.cgColor
+        view4.layer.cornerRadius = 20
+        view4.layer.borderWidth = 3
+        view4.layer.borderColor = UIColor.black.cgColor
+        item4.layer.cornerRadius = 30
+        item4.layer.borderWidth = 3
+        item4.layer.borderColor = UIColor.black.cgColor
         
     }
-    @objc func rightBarButtonTap() {
-        let alert = UIAlertController(title: "New zametrki", message: "", preferredStyle: .alert)
-        present(alert, animated: true, completion: nil)
-    }
+
+
 }
-        extension ViewController: UITableViewDataSource {
-            func tableView(_ tableView: UITableView, numberOfRowsInSection sections: Int) -> Int{
-                return zametki.count
-            }
-            func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-                let cell = tableView.dequeueReusableCell(withIdentifier: "zametka_cell", for: indexPath)
-                cell.textLabel?.text = zametki[indexPath.row]
-                
-                return cell
-            }
-}
+
+
+
+
 
 
